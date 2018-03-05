30fc839e46c88526f1eb6e9169fa392d364376b0
Haruna <haruna0510@kif.biglobe.ne.jp>
2018-03-06 08:43:03 +0900

    stage

--- 070. Climbing Stairs/scripts.js
+++ 070. Climbing Stairs/scripts.js
@@ -1,3 +1,13 @@
 
 // 70. Climbing Stairs
-// start from reading answers
+const climbStairs = function (n) {
+    let arr = [],
+        i
+    arr[0] = 0
+    arr[1] = 1
+    arr[2] = 2
+    for (i = 3; i <= n; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2]
+    }
+    return arr[n]
+}
